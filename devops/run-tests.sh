5ee0e2d7a426ce415dd54b5073807a9458a0d9bb
Emad Zaamout <50178852+eezaamout@users.noreply.github.com>
2021-09-26 18:06:40 -0600

    Update run-tests.sh

--- devops/run-tests.sh
+++ devops/run-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Author Emad Zaamout | support@ahtcloud.com
 # Start server & give time to start.
 # & flag used to start server and continue, otherwise it will pause.
 php artisan serve &
